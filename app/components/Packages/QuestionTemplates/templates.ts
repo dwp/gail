66f5c99217e644c798047aaffcd527c0d1830985
Elsa Wadha <elsa.wadha@engineering.digital.dwp.gov.uk>
2025-11-25 14:13:46 +0000

    Update templates.ts
    GitOrigin-RevId: 70ea97f61aeb62e3993f434bcde53c90e76387ec

--- app/components/Packages/QuestionTemplates/templates.ts
+++ app/components/Packages/QuestionTemplates/templates.ts
@@ -1,8 +1,8 @@
 const templates = [
-  "What is a claimant's eligibility for ...",
-  "What is required to support a claim in ...",
-  "How long does the ... process typically take",
-  "What are common reasons for ... claim denial",
+  "How does a claimant apply for [text here]",
+  "What are the eligibility criteria for [text here]",
+  "How can a claimant [text here] ",
+  "Tell me more about [text here]",
 ];
 
 export { templates };
