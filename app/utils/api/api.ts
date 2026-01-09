bf9956af3100f24282792166c5ae52c6203c26aa
Elsa Wadha <elsa.wadha@engineering.digital.dwp.gov.uk>
2026-01-09 10:19:02 +0000

    update api.ts
    GitOrigin-RevId: 925fc5bab4dfe6d6206458fd74599e09ca331d93

--- app/utils/api/api.ts
+++ app/utils/api/api.ts
@@ -1,6 +1,10 @@
 import { sendQueryMessage } from "./";
 
-export const sendQuery = async (query: string, location: string) => {
-  const chat_history = sendQueryMessage(query, location);
+export const sendQuery = async (
+  query: string,
+  location: string,
+  counter: number,
+) => {
+  const chat_history = sendQueryMessage(query, location, counter);
   return chat_history;
 };
