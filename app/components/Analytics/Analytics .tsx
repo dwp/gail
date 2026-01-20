5909dba91208a92d37f22318eb8e509e4cdf53cc
Elsa Wadha <elsa.wadha@engineering.digital.dwp.gov.uk>
2026-01-20 14:14:36 +0000

    ranj updates
    GitOrigin-RevId: 942401c5b6000404a76a040c017b21c9fbed43fa

--- app/components/Analytics/Analytics .tsx
+++ app/components/Analytics/Analytics .tsx
@@ -3,14 +3,6 @@ import Script from "next/script";
 export default function Analytics() {
   return (
     <>
-      <Script strategy="beforeInteractive" id="g-analytics">
-        {`
-        dataLayer = [{
-            'gtm.allowlist': ['gaawc', 'gaawe', 'ua', 'cl', 'lcl', 'fsl', 'tl', 'jel', 'hl', 'v', 'c', 'ctv', 'e', 'dbg', 'd',
-            'f', 'j', 'smm', 'r' ,'u', 'gas', 'remm', 'vis', 'evl' , 'sdl', 'opt' ]
-        }];
-        `}
-      </Script>
       <Script strategy="beforeInteractive" id="g-analytics-heroku">
         {`
         <script>
--- app/layout.tsx
+++ app/layout.tsx
@@ -18,14 +18,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="govuk-template__body govuk-frontend-supported">
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-PTF5XGF5"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          ></iframe>
-        </noscript>
         {/* Google Tag Manager (noscript) HEROKU */}
         <noscript>
           <iframe
@@ -36,13 +28,6 @@ export default function RootLayout({
           ></iframe>
         </noscript>
         {/* End Google Tag Manager (noscript) HEROKU */}
-        <Script strategy="afterInteractive" id="g-analytics">
-          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-PTF5XGF5');`}
-        </Script>
         <Providers>
           <SkipLink />
           <Layout>{children}</Layout>
